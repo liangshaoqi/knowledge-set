48381bb5aa631e5dd68892e0d202d3918e583e83
LiangShaoQi <liangshaoqi@shixinyun.com>
2019-05-08 16:35:37 +0800

    [update] 重新初始化项目

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-import './assets/styles/style.css'
-console.log('我是indexjs')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
