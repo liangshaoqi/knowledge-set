e3f45cf9cb31068f4963fdd5fcd3019e3cd8eb7b
liangshaoqi <918872153@qq.com>
2019-05-11 16:26:41 +0800

    添加

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom'
+import { Route, Switch, BrowserRouter, Redirect } from 'react-router-dom'
 import Layout from '@view/layout'
 import Login from '@view/login'
 export default class Crouter extends Component {
@@ -7,6 +7,10 @@ export default class Crouter extends Component {
     return (
       <BrowserRouter>
         <Switch>
+          <Route exact path='/' render={() => {
+            return <Redirect to='/layout'></Redirect>
+          }}>
+          </Route>
           <Route path='/layout' component={Layout}></Route>
           <Route path='/login' component={Login}></Route>
         </Switch>
