e5ab11ae890421395a3d84c7a9b388408497a442
梁绍奇 <918872153@qq.com>
2020-07-08 15:24:54 +0800

    [update] 遮罩和图片帧动画

--- src/view/cssStyle/mask/mask.jsx
+++ src/view/cssStyle/mask/mask.jsx
@@ -3,6 +3,11 @@ import './mask.scss'
 export default function Mask() {
   return (
     <div className='mask-view'>
+      <p>mask使用png图片会遮挡住实际的图片</p>
+      <div className='mask_0'>
+        <p>基本用法,</p>
+        <div className='mask_bg'></div>
+      </div>
       <div className='mask_1'>
         <p>基本用法</p>
         <div className='mask_bg'></div>
@@ -26,6 +31,21 @@ export default function Mask() {
       <div className='mask_6'>
         <p>图片角度渐变</p>
         <div className='mask_bg'></div>
+      </div>
+      <div className='mask_7'>
+        <p>mask&滤镜</p>
+        <div className='mask_bg'></div>
+      </div>
+      <div className='mask_8'>
+        <p>mask&滤镜</p>
+        <div className='mask_bg'></div>
+      </div>
+      <div className='mask_9'>
+        <p>mask&滤镜,利用png图片进行遮罩</p>
+        <div className='mask_bg'>
+          <div className='img1'></div>
+          <div className='img2'></div>
+        </div>
       </div>
     </div>
   )
--- src/view/workbook/animate/index.jsx
+++ src/view/workbook/animate/index.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import './index.scss'
 export default function Animate() {
   return (
-    <div>动画</div>
+    <div className='animation-view'>
+      <div className='loading-view'>
+        <p>动画steps使用,steps ( n, [start | end] ),第一个参数是一个正值，指定动画分割的段数
+第二个参数定义动画执行开始点，可设定为 start 或 end，这个值为可选值，当未传入参数时默认以 end 方式执行</p>
+        <img className='loading' src={require('../../../assets/work/loading.png')} alt='loading'></img>
+      </div>
+      <div className='person-run'>
+        <p>人物奔跑使用background-position属性控制</p>
+        <div className='person'></div>
+        <div className='person1'></div>
+      </div>
+    </div>
   )
 }
